f60bb2d1a635f8a6b29de304b1351df8507f7b03
dejager-mc <dejager.mc@gmail.com>
2016-02-25 20:45:08 +0100

    dao config

--- domain/src/main/java/nl/ocwduo/vzub/config/ApplicationContext.java
+++ domain/src/main/java/nl/ocwduo/vzub/config/ApplicationContext.java
@@ -1,8 +1,14 @@
 package nl.ocwduo.vzub.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 /**
  * Created by Machiel de Jager on 20-2-2016.
@@ -11,4 +17,21 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories
 @EnableTransactionManagement
 public class ApplicationContext {
+
+    @Bean
+    public DataSource dataSourceVzub() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName();
+        dataSource.setUrl();
+        dataSource.setUsername();
+        dataSource.setPassword();
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(dataSourceVzub());
+        return transactionManager;
+    }
 }
